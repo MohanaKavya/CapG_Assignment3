a1415899594d043a0cd8f8204d622a2bf844be1d
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-09-25 19:00:50 +0530

    Added : Comparison of Two Lines with equals() Method is Added

--- LineComparison.java
+++ LineComparison.java
@@ -4,23 +4,35 @@ public class LineComparison {
 
 	public static void main(String[] args) {
 		// variables
-		double length_of_line = 0;
+		double length_of_line_1 = 0;
+		double length_of_line_2 = 0;
 		
 		// Display Welcome Message
 		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
 		
-		// Co-ordinates Generation for 2 Points on a Line
+		// Co-ordinates Generation for 4 Points on Lines 1 & 2
 		int x1 = (int)(Math.floor(Math.random()*10)); 
 		int y1 = (int)(Math.floor(Math.random()*10));	
 		int x2 = (int)(Math.floor(Math.random()*10)); 
-		int y2 = (int)(Math.floor(Math.random()*10)); 
+		int y2 = (int)(Math.floor(Math.random()*10));
+		int x3 = (int)(Math.floor(Math.random()*10)); 
+		int y3 = (int)(Math.floor(Math.random()*10));	
+		int x4 = (int)(Math.floor(Math.random()*10)); 
+		int y4 = (int)(Math.floor(Math.random()*10));
 		
-		// Calculation of Length of Line based on Co-ordinates Generated
-		length_of_line = Math.sqrt( (x2 - x1) * (x2 - x1)  + (y2- y1) * (y2- y1) ); 
+		// Calculation of Length of Lines 1 & 2 based on Co-ordinates Generated
+		length_of_line_1 = Math.sqrt( (x2 - x1) * (x2 - x1)  + (y2- y1) * (y2- y1) ); 
+		length_of_line_2 = Math.sqrt( (x4 - x3) * (x4 - x3)  + (y4- y3) * (y4- y3) );
 		
-		// Display of Line Length
-		System.out.println("Length of the Line : "+Math.round(length_of_line * 100.0)/100.0);
-
+		// Double Objects Creation for Lines Length
+		Double Line_1 = new Double(length_of_line_1);
+		Double Line_2 = new Double(length_of_line_2);
+		
+		// Comparison of Two Lines using equals() Method and Display
+		if(Line_1.equals(Line_2))
+			System.out.println("Both the Lines are Equal");
+		else
+			System.out.println("Both the Lines are Not Equal");	
 
 	}
 
