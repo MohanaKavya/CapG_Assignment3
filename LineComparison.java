1ad0ae68c9ae217b92ebf2b615ac4802d5df1057
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-09-25 20:34:46 +0530

    Refactor : Use OOPS Concepts for Line Comparison Computation is Refactored

--- LineComparison.java
+++ LineComparison.java
@@ -1,11 +1,54 @@
 package AssignmentThree;
 
 public class LineComparison {
+		
+		// variables
+		private double length_of_line;
+		private int x1, x2, y1, y2;
+		
+		// Parameterized Constructor
+		public LineComparison(int x1, int y1, int x2, int y2)
+		{
+			this.x1 = x1;
+			this.y1 = y1;
+			this.x2 = x2;
+			this.y2 = y2;
+		}
+	
+		// Calculation of Length of Lines 1 & 2 based on Co-ordinates Generated
+		public double lengthOfLine() {
+			length_of_line = Math.sqrt( (x2 - x1) * (x2 - x1)  + (y2- y1) * (y2- y1) ); 
+			return Math.round(length_of_line * 100.0)/100.0;
+		}
+		
+		// Overriding equals() Method
+		public boolean equals(LineComparison Line_2)
+		{	boolean equalityValue;
+		
+			if(this.lengthOfLine() == Line_2.lengthOfLine())
+				equalityValue = true;
+			else
+				equalityValue = false;
+				
+			return equalityValue;
+		}
+		
+		
+		// Overriding compareTo() Method
+		public int compareTo(LineComparison Line_2) {
+	        	int compareValue;
+	        
+	        	if(this.lengthOfLine() == Line_2.lengthOfLine())
+	        		compareValue = 0;
+	        	else if(this.lengthOfLine() > Line_2.lengthOfLine())
+	        		compareValue = 1;
+	       	 	else compareValue = -1;
+	        
+	        	return compareValue;
+		}
+	    	
 
 	public static void main(String[] args) {
-		// variables
-		double length_of_line_1 = 0;
-		double length_of_line_2 = 0;
 		
 		// Display Welcome Message
 		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
@@ -20,23 +63,25 @@ public class LineComparison {
 		int x4 = (int)(Math.floor(Math.random()*10)); 
 		int y4 = (int)(Math.floor(Math.random()*10));
 		
-		// Calculation of Length of Lines 1 & 2 based on Co-ordinates Generated
-		length_of_line_1 = Math.sqrt( (x2 - x1) * (x2 - x1)  + (y2- y1) * (y2- y1) ); 
-		length_of_line_2 = Math.sqrt( (x4 - x3) * (x4 - x3)  + (y4- y3) * (y4- y3) );
+		// Instantiating Objects
+		LineComparison Line_1 = new LineComparison(x1, y1, x2, y2);
+		LineComparison Line_2 = new LineComparison(x3, y3, x4, y4);
 		
-		// Double Objects Creation for Lines Length
-		Double Line_1 = new Double(length_of_line_1);
-		Double Line_2 = new Double(length_of_line_2);
+		boolean equality_check = Line_1.equals(Line_2);
+		int compare_value = Line_1.compareTo(Line_2);
 		
-		// Comparison of Two Lines using compareTo() Method and Display
-	        int compareValue = Line_1.compareTo(Line_2); 
-        	if (compareValue == 0) 
-            		System.out.println("Lengths of Line 1 and Line 2 are equal"); 
-       	 	else if (compareValue < 0) 
-            		System.out.println("Length of Line 1 is less than Line 2"); 
-        	else
-            		System.out.println("Length of Line 1 is greater than Line 2");
-
+		// Display if Two are Equal or Not Equal
+		if(equality_check)
+			System.out.println("Both the Lines are Equal");
+		else
+			System.out.println("Both the Lines are Not Equal");	
+		
+		// Display of Comparison of Two lines
+		if (compare_value > 0)
+			System.out.println("Length of Line 1 is greater than Line 2");
+		else if (compare_value < 0) 
+    			System.out.println("Length of Line 1 is less than Line 2"); 
+		
 	}
 
 }
