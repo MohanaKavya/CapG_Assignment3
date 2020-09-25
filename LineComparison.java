4eba895f66482823665faae824959884cda8ebaa
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-09-25 16:50:44 +0530

    Added : Display of Welcome Message to Line Comparison is Added

---
+++ LineComparison.java
@@ -0,0 +1,12 @@
+package AssignmentThree;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		
+		// Display Welcome Message
+		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+
+	}
+
+}
